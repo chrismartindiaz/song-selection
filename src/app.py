8cc3445adedb39e1961677e3628492bdd9ee628e
Christian Martín Díaz <146923531+chrismartindiaz@users.noreply.github.com>
2024-02-18 22:58:29 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 
 # Cargar el modelo de predicción de 'mood'
 lr_model = joblib.load('modelo_lr_mood.pkl')
