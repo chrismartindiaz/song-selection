525db5e5194e6618ec03751a758056d93157a3aa
Christian Martín Díaz <146923531+chrismartindiaz@users.noreply.github.com>
2024-02-19 17:05:07 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -38,7 +38,7 @@ if st.button("¡Sorpréndeme!"):
         # Generar iframes de Spotify con los ID de las canciones
         for index, song in selected_songs.iterrows():
             iframe_code = f"""
-            <iframe allowtransparency="true" style="border-radius:12px; background-color: transparent;" 
+            <iframe style="border-radius:12px; background-color: transparent;" 
             src="https://open.spotify.com/embed/track/{song['id']}?utm_source=generator" 
             width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay"; clipboard-write; 
             encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
